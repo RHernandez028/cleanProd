9e1ee394e4685e4427862dad3dc85b5765e11119
Gabriel <rhernadnez028@west-mec.org>
2020-03-13 17:26:25 -0700

    prod pages

--- app.js
+++ app.js
@@ -2,91 +2,57 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const ejs = require('ejs');
-
 app.use(express.static('public'));
 // app.use(expressLayouts)
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
 app.get('/', (req, res) => {
     try {
         res.render('index', {
-            
         })
     } catch (error) {
-        
     }
 });
-
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-app.get('/', (req, res) => {
-=======
 app.get('/gloves', (req, res) => {
->>>>>>> master
     try {
         res.render('gloves', {
-            
         })
     } catch (error) {
-        
     }
 });
-
 app.get('/hand-sanitizer', (req, res) => {
     try {
         res.render('handsan', {
-            
         })
     } catch (error) {
-        
     }
 });
-
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
-=======
 app.get('/surgical-masks', (req, res) => {
     try {
         res.render('masks', {
-            
         })
     } catch (error) {
-        
     }
 });
-
 app.get('/soap', (req, res) => {
     try {
         res.render('soap', {
-            
         })
     } catch (error) {
-        
     }
 });
-
 app.get('/spray', (req, res) => {
     try {
         res.render('soap', {
-            
         })
     } catch (error) {
-        
     }
 });
->>>>>>> master
-
 app.get('/wipes', (req, res) => {
     try {
         res.render('wipes', {
-            
         })
     } catch (error) {
-        
     }
 });
-
 app.listen(2002);
